09a77b838c0dbf70c3580a2881f7f0a2421a1954
Ringo Hoffmann <riho@live.de>
2021-02-21 11:56:56 +0100

    remove web app and migrator

--- WebApp/src/util/time.ts
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-export const NULL_DATE = '0001-01-01T00:00:00';
-
-export default class TimeUtil {
-  public static readonly nullDate = new Date('0001-01-01T00:00:00');
-}
--- WebApp/src/util/urls.ts
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-export default class UrlUtils {
-  public static formatDestination(url: string): string {
-    return url.replace(/(?:https?:\/\/)?(?:www\.)?/g, '');
-  }
-}
